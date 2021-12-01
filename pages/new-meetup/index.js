8367d082d17966f2f2905e7c2e62f0850a07db5e
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-12-01 19:45:29 +0330

    add MongoDB Atlas Database

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,18 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    router.push("/");
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
