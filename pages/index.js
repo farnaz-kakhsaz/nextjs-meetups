aefa182d6582ddd4ab4e0f97bab5f3810984115e
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-12-28 21:40:13 +0330

    move revalidate to main scope of object

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export async function getStaticProps() {
         description: meetup.description,
         id: meetup._id.toString(),
       })),
-      revalidate: 1,
     },
+    revalidate: 1,
   };
 }
